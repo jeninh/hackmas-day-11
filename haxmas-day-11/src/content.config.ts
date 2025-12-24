c8eba85ea349df608d2af2701861ead96b8d0f0c
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2025-12-24 00:55:11 +0000

    oopsies

--- haxmas-day-11/src/content.config.ts
+++ haxmas-day-11/src/content.config.ts
@@ -10,7 +10,6 @@ const blog = defineCollection({
             title: z.string(),
             description: z.string(),
             pubDate: z.coerce.date(),
-            updatedDate: z.coerce.date().optional(),
             heroImage: image().optional(),
         }),
 });
